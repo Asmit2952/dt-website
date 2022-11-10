2ca062d0151f18d886fecc3d2e2e224b39acffd6
Asmit2952 <asmitbm2952002@gmail.com>
2022-11-10 21:58:34 +0530

    add cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>Cta</div>
+    <div className="gpt3_cta">
+      <div className="gpt3_cta_content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3_cta_btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
